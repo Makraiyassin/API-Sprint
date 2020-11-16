c4e4e9bd6329a54df66ee08f90e17ec2386d0c17
makrai <makrai.yassin@gmail.com>
2020-11-16 11:54:19 +0100

    gif

--- script.js
+++ script.js
@@ -2,7 +2,16 @@ document.querySelector("#button").addEventListener("click", function(){
     fetch("https://dog.ceo/api/breeds/image/random")
     .then( response => response.json())
     .then( (data) => {
-        document.getElementById("button").insertAdjacentHTML("afterend",'<br><br><img src ="'+data.message+'">')
+        document.querySelector("img").setAttribute("src",data.message)
+        // document.getElementById("button").insertAdjacentHTML("afterend",'<br><br><img width = "400px" style = "margin-left: 10%" src ="'+data.message+'">')
         console.log(data);
     })
 })
+
+// document.querySelector("#button").addEventListener("click", function(){
+    // fetch("https://anapioficeandfire.com/api/characters/583")
+    // .then( response => response.json())
+    // .then( (data) => {
+    //     console.log(data);
+    // })
+// })
